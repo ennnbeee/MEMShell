5b25b6504a649102bee78ba36a6ea4708f2b8feb
ennnbeee <nickjbenton@gmail.com>
2023-02-18 12:46:12 +0000

    variable change

--- Build.ps1
+++ Build.ps1
@@ -1,33 +1,13 @@
-param (
-    [string] $buildVersion,
-    [string] $apiKey
+param(
+    [version]$Version
 )
+#Requires -Module ModuleBuilder
 
-$Global:ErrorActionPreference = 'Stop'
-$Global:VerbosePreference = 'SilentlyContinue'
-
-$manifestPath = './MEMShell/MEMShell.psd1'
-$publicFuncFolderPath = './MEMShell'
-
-if (!(Get-PackageProvider | Where-Object { $_.Name -eq 'NuGet' })) {
-    Install-PackageProvider -Name NuGet -Force | Out-Null
-}
-Import-PackageProvider -Name NuGet -Force | Out-Null
-
-if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+$Params = @{
+    SourcePath = "$PSScriptRoot\Source\MEMShell.psd1"
+    #CopyPaths = @("$PSScriptRoot\README.md")
+    Version = $Version
+    #UnversionedOutputDirectory = $true
 }
 
-$manifestContent = (Get-Content -Path $manifestPath -Raw) -replace "'<ModuleVersion>'", $buildVersion
-
-if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Recurse -Filter '*.ps1' | Select-Object -ExpandProperty BaseName)) {
-    $FuncStrings = "'$($publicFunctionNames -join "','")'"
-}
-else {
-    $FuncStrings = $null
-}
-
-$manifestContent = $manifestContent -replace "'<FunctionsToExport>'", $funcStrings
-$manifestContent | Set-Content -Path $manifestPath
-
-Publish-Module -Path ./MEMShell -NuGetApiKey $apiKey -Verbose
+Build-Module @params
